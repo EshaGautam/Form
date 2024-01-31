10ac3840dc96505d1c74b70484787aa54c7d0751
Esha Gautam <eshagautam0@gmail.com>
2024-01-31 21:09:18 +0530

    form validation using useEffect hook

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  
+useEffect(()=>{
+  let storeLoggedInKey = localStorage.getItem("loggedIn");
+   if(storeLoggedInKey==='1'){
+    setIsLoggedIn(true)
+}},[])
+ 
+  
+
+
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem('loggedIn','1')
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+     localStorage.clear()
+     }
+  
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
